66b7e2bf326c52733ed1fa29692877fcdaaf6ed5
AAArmand <armand.airapetyan@yandex.ru>
2018-05-03 21:27:26 +0500

    Add picture generation

--- ML.Experience/CSVToImages.cs
+++ ML.Experience/CSVToImages.cs
@@ -1,5 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Accord.IO;
+using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,45 +11,30 @@ namespace ML.Experience
 {
     class CSVToImages
     {
-        using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ML.Experience
-    {
-        class CSVToImages
+        public CSVToImages(string path)
         {
+            int[][] data = new CsvReader(path, true).ToJagged<int>();
 
-            public CSVToImages(string path)
+            //создаем объект Bitmap, размеры картинки - квадратный корень из количества пикселей в csv
+            Bitmap img = new Bitmap((int)Math.Sqrt(data[0].Length), (int)Math.Sqrt(data[0].Length));
+            foreach (int[] rowOfPixels in data)
             {
-                int[][] data = new CsvReader(path, true).ToJagged<int>();
 
-                //создаем объект Bitmap, размеры картинки - квадратный корень из количества пикселей в csv
-                Bitmap img = new Bitmap((int)Math.Sqrt(data[0].Length), (int)Math.Sqrt(data[0].Length));
-                foreach (int[] rowOfPixels in data)
+                for (int i = 1; i < rowOfPixels.Length; i++)
                 {
-
-                    for (int i = 1; i < rowOfPixels.Length; i++)
+                    for (int Xcount = 0; Xcount < img.Width; Xcount++)
                     {
-                        for (int Xcount = 0; Xcount < img.Width; Xcount++)
+                        for (int Ycount = 0; Ycount < img.Height; Ycount++)
                         {
-                            for (int Ycount = 0; Ycount < img.Height; Ycount++)
-                            {
-                                if (rowOfPixels[i] == 0)
-                                {
-                                    rowOfPixels[i] = 255;
-                                }
-                                img.SetPixel(Xcount, Ycount, Color.FromArgb(0, rowOfPixels[i], rowOfPixels[i], rowOfPixels[i]));
-                            }
+                            img.SetPixel(Xcount, Ycount, Color.FromArgb(0, rowOfPixels[i], rowOfPixels[i], rowOfPixels[i]));
                         }
                     }
-                    img.Save(@"C:\Projects\ML.Experience-master\Data\Mnist\ResultImages\" + rowOfPixels[0] + "__" + RandomToken.RandomString(30) + ".jpg");
                 }
+                img.Save(@"C:\Projects\ML.Experience-master\Data\Mnist\ResultImages\" + rowOfPixels[0] + "__" + RandomToken.RandomString(30) + ".jpg");
             }
         }
     }
 }
-}
+
+
