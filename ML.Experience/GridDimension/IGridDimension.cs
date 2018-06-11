389b470d9c51139edf86fb56592c9b3674d21c8a
Kopyl <dimakopylov98@mail.ru>
2018-06-11 20:58:45 +0500

    updated code

--- ML.Experience/GridDimension/IGridDimension.cs
+++ ML.Experience/GridDimension/IGridDimension.cs
@@ -8,7 +8,6 @@ namespace ML.Experience.GridDimension
         IClassifierLearn Classifier { get; set; }
         object Value { get; }
         int LengthCriterion { get; }
-        int Count { get; set; }
         void Reset();
         void Next();
         void Back();
